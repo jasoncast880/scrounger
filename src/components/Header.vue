647d0cf72f9a7d0c5a5dcffd4dcfb95ac3f6f0e3
plbesque <jasoncast880@gmail.com>
2023-04-19 06:46:09 -0400

    updated frontend: april 19

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,35 @@
 <template>
     <header>
-        <h1>{{title}}</h1>
+        <h1>{{ title }}</h1>
+        <Button text = "Add Task" color="green" />
     </header>
+    <br>
+    <body>
+        <p><i>Another mans trash is your treasure!</i></p>
+    </body>
 </template>
 
 <script>
+    import Button from './Button'
+
 export default{
     name: 'Header',
     props: {
-        type: String,
+        title: String,
+        headerText: String,
     },
-
+    components: {
+        Button
+    }    
 }
 </script>
 
 <style scoped>
-header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: bottom 20px;
-}
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: bottom 20px;
+    }
+
 </style>
